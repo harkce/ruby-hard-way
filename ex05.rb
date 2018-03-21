4d3cc4e5a120a2d7995bce64837ed56cb98ca194
Habib Fikri <habibgarut@gmail.com>
2018-03-21 16:45:56 +0700

    More variables and printing

---
+++ ex05.rb
@@ -0,0 +1,16 @@
+my_name = 'Habib Fikri'
+my_age = 22
+my_height = 160
+my_weight = 56
+my_eyes = 'coklat'
+my_teeth = 'putih'
+my_hair = 'hitam'
+
+puts "Kita berbicara tentang #{my_name}."
+puts "Tingginya #{my_height} cm."
+puts "Beratnya #{my_weight} kg."
+puts 'Sebenarnya, itu tidak terlalu berat.'
+puts "Matanya berwarna #{my_eyes} dan berambut #{my_hair}"
+puts "Giginya selalu berwarna #{my_teeth}"
+
+puts "Tambahkan #{my_age}, #{my_height}, dan #{my_weight} hasilnya #{my_age + my_height + my_weight}."
