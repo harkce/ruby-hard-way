c1a34dca61ac1a90950eeb98299d9d09d5afe77b
Habib Fikri <habibgarut@gmail.com>
2018-03-21 16:45:33 +0700

    Numbers and math

---
+++ ex03.rb
@@ -0,0 +1,27 @@
+puts 'plus'
+puts "1 + 1 = #{1 + 1}"
+
+puts ''
+
+puts 'minus'
+puts "2 - 6 = #{2 - 6}"
+
+puts ''
+
+puts 'slash'
+puts "3 / 2 = #{3 / 2}"
+
+puts ''
+
+puts 'asterik'
+puts "4 * 2 = #{4 * 2}"
+
+puts ''
+
+puts 'obelisk'
+puts "9 % 4 = #{9 % 4}"
+
+puts ''
+
+puts 'in the black box'
+puts "5 < 3 ? #{5 < 3}"
